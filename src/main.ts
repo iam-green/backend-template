572361c17f314015a0b6a64b3ecf632752ecfaf8
iam-green <yundohyun050121@gmail.com>
2025-02-08 06:59:05 +0900

    edit: `main.ts`
    global pipe and swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import 'dotenv/config';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  app.useGlobalPipes(new ZodValidationPipe());
+  if (process.env.NODE_ENV != 'production') {
+    patchNestJsSwagger();
+    const config = new DocumentBuilder()
+      .setTitle('Backend Template')
+      .setDescription('Backend Template')
+      .setVersion('0.0.1')
+      .addOAuth2()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
