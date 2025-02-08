00af5bc67fd276a975dc0cab06acb098b9433cd6
iam-green <yundohyun050121@gmail.com>
2025-02-08 09:45:18 +0900

    add: `auth` controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleAuth() {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthCallback(@Req() req) {
+    // TODO: Register or login user
+  }
+}
