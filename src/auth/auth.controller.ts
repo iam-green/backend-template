2740ace41a14b9bd4af1ec9d431a00c16dd43a80
iam-green <yundohyun050121@gmail.com>
2025-02-10 08:17:41 +0900

    edit: `auth` controller
    add: `googleAuthCallback` function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,46 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { IGoogleUser } from './interface/google-user.interface';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
   @Get('google')
   @UseGuards(AuthGuard('google'))
   googleAuth() {}
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  googleAuthCallback(@Req() req) {
-    // TODO: Register or login user
+  async googleAuthCallback(
+    @Req()
+    req: Request & { user: IGoogleUser },
+    @Res() res: Response,
+  ) {
+    const user = await this.authService.googleLogin(
+      req.user.id,
+      req.user.email,
+    );
+
+    const { accessToken, refreshToken } = this.authService.generateToken({
+      id: user.id,
+    });
+
+    res.cookie('token', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV == 'production',
+      maxAge: this.configService.get<number>(
+        'REFRESH_TOKEN_EXPIRES_IN',
+        1000 * 60 * 60 * 24 * 30,
+      ),
+    });
+
+    res.status(200).json({ ...user, accessToken });
   }
 }
