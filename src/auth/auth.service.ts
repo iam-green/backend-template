b7652a86bdd3355bb68ce131ae68e75c59e68841
iam-green <yundohyun050121@gmail.com>
2025-02-10 08:17:12 +0900

    edit: `auth` service
    add `googleLogin` function
    edit token expires in config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { user } from 'src/user/user.schema';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
+    private readonly userService: UserService,
   ) {}
 
   generateToken(payload: { id: string }) {
     const accessToken = this.jwtService.sign(payload, {
       secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
-      expiresIn: this.configService.get<string>('ACCESS_TOKEN_EXPIRES_IN'),
+      expiresIn: this.configService.get<number>(
+        'ACCESS_TOKEN_EXPIRES_IN',
+        1000 * 60 * 60,
+      ),
     });
     const refreshToken = this.jwtService.sign(payload, {
       secret: this.configService.get<string>('REFRESH_TOKEN_SECRET'),
-      expiresIn: this.configService.get<string>('REFRESH_TOKEN_EXPIRES_IN'),
+      expiresIn: this.configService.get<number>(
+        'REFRESH_TOKEN_EXPIRES_IN',
+        1000 * 60 * 60 * 24 * 30,
+      ),
     });
     return { accessToken, refreshToken };
   }
@@ -45,5 +55,10 @@ export class AuthService {
     const decoded = this.verifyRefreshToken(refreshToken);
     if (!decoded) return null;
     return this.generateToken(decoded as { id: string });
+  }
+
+  async googleLogin(id: string, email: string) {
+    const user = await this.userService.getByGoogleId(id);
+    return user ?? this.userService.create({ google_id: id, email });
   }
 }
