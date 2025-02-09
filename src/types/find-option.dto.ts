a5296cb4d4102fd8e0cd5ecf97c0a2dcbd67ff40
iam-green <yundohyun050121@gmail.com>
2025-02-10 07:36:24 +0900

    edit: `find-option` dto
    null, undefined 값 허용

--- src/types/find-option.dto.ts
+++ src/types/find-option.dto.ts
@@ -1,17 +1,27 @@
 import { z } from 'zod';
 
 export const FindOptionDto = z.object({
-  sort: z.enum(['asc', 'desc']).default('asc'),
-  page: z.number().default(1),
+  sort: z
+    .enum(['asc', 'desc'])
+    .nullish()
+    .default('asc')
+    .transform((v) => v ?? 'asc'),
+  page: z
+    .number()
+    .nullish()
+    .default(1)
+    .transform((v) => v ?? 1),
   limit: z.number().default(10),
   from: z.coerce
     .date()
     .or(z.coerce.number())
-    .transform((v) => new Date(v))
-    .default(new Date(0)),
+    .nullish()
+    .default(new Date(0))
+    .transform((v) => new Date(v ?? 0)),
   to: z.coerce
     .date()
     .or(z.coerce.number())
-    .transform((v) => new Date(v))
-    .default(() => new Date()),
+    .nullish()
+    .default(() => new Date())
+    .transform((v) => new Date(v ?? new Date())),
 });
