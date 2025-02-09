63d4c32afe38942122f3f176fa3033a352493b62
iam-green <yundohyun050121@gmail.com>
2025-02-10 07:54:43 +0900

    edit: `auth` module
    add `user` module import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategy/google.strategy';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PassportModule.register({}), JwtModule],
+  imports: [PassportModule.register({}), JwtModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
   exports: [AuthService],
