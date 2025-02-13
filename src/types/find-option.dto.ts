4d2737c9af45b94aac96f2c7e8384db8b4d1bc79
iam-green <yundohyun050121@gmail.com>
2025-02-14 00:13:18 +0900

    edit: `find-option` dto

--- src/types/find-option.dto.ts
+++ src/types/find-option.dto.ts
@@ -1,17 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
-export const FindOptionDto = z.object({
+export const findOptionSchema = z.object({
   sort: z
     .enum(['asc', 'desc'])
     .nullish()
     .default('asc')
     .transform((v) => v ?? 'asc'),
-  page: z
+  page: z.coerce
     .number()
     .nullish()
     .default(1)
     .transform((v) => v ?? 1),
-  limit: z.number().default(10),
+  limit: z.coerce.number().default(10),
   from: z.coerce
     .date()
     .or(z.coerce.number())
@@ -25,3 +27,42 @@ export const FindOptionDto = z.object({
     .default(() => new Date())
     .transform((v) => new Date(v ?? new Date())),
 });
+
+export class FindOptionDto extends createZodDto(findOptionSchema) {
+  @ApiProperty({
+    description: 'Sort order',
+    enum: ['asc', 'desc'],
+    required: false,
+  })
+  sort: 'asc' | 'desc' = 'asc';
+
+  @ApiProperty({
+    description: 'Page number',
+    default: 1,
+    minimum: 1,
+    required: false,
+  })
+  page: number = 1;
+
+  @ApiProperty({
+    description: 'Number of items per page',
+    default: 10,
+    minimum: 1,
+    required: false,
+  })
+  limit: number = 10;
+
+  @ApiProperty({
+    description: 'From date',
+    default: new Date(0),
+    required: false,
+  })
+  from: Date = new Date(0);
+
+  @ApiProperty({
+    description: 'To date',
+    default: () => new Date(),
+    required: false,
+  })
+  to: Date = new Date();
+}
