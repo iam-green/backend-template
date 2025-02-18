f9da9da6b6b3122c7e24b27614b07b9ecbd3c198
iam-green <yundohyun050121@gmail.com>
2025-02-19 02:24:44 +0900

    edit: `auth` controller
    discord 관련 요청 추가 및 swagger 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,6 +12,8 @@ import { IGoogleUser } from './interface/google-user.interface';
 import { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { ApiOperation } from '@nestjs/swagger';
+import { IDiscordUser } from './interface/discord-user.interface';
+import { UserDto } from 'src/user/dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -20,15 +22,28 @@ export class AuthController {
     private readonly configService: ConfigService,
   ) {}
 
+  @Get()
+  @ApiOperation({
+    summary: 'Auth',
+    description: '현재 로그인 되어 있는 계정 확인',
+  })
+  @UseGuards(AuthGuard('jwt'))
+  auth(@Req() req: Request & { user: UserDto }) {
+    return req.user;
+  }
+
   @Get('google')
-  @ApiOperation({ summary: 'Google Auth', description: 'Google Auth' })
+  @ApiOperation({
+    summary: 'Google OAuth',
+    description: 'Google OAuth를 사용하여 로그인',
+  })
   @UseGuards(AuthGuard('google'))
   googleAuth() {}
 
   @Get('google/callback')
   @ApiOperation({
-    summary: 'Google Auth Callback',
-    description: 'Google Auth Callback',
+    summary: 'Google OAuth Callback',
+    description: 'Google OAuth Callback 데이터 받기',
   })
   @UseGuards(AuthGuard('google'))
   async googleAuthCallback(
@@ -57,8 +72,50 @@ export class AuthController {
     res.status(200).json({ accessToken });
   }
 
+  @Get('discord')
+  @ApiOperation({
+    summary: 'Discord OAuth',
+    description: 'Discord OAuth를 사용하여 로그인',
+  })
+  @UseGuards(AuthGuard('discord'))
+  discordAuth() {}
+
+  @Get('discord/callback')
+  @ApiOperation({
+    summary: 'Discord OAuth Callback',
+    description: 'Discord OAuth Callback 데이터 받기',
+  })
+  @UseGuards(AuthGuard('discord'))
+  async discordAuthCallback(
+    @Req() req: Request & { user: IDiscordUser },
+    @Res() res: Response,
+  ) {
+    const user = await this.authService.discordLogin(
+      req.user.id,
+      req.user.email,
+    );
+
+    const { accessToken, refreshToken } = this.authService.generateToken({
+      id: user.id,
+    });
+
+    res.cookie('token', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV == 'production',
+      maxAge: this.configService.get<number>(
+        'REFRESH_TOKEN_EXPIRES_IN',
+        1000 * 60 * 60 * 24 * 30,
+      ),
+    });
+
+    res.status(200).json({ accessToken });
+  }
+
   @Get('refresh')
-  @ApiOperation({ summary: 'Refresh Token', description: 'Refresh Token' })
+  @ApiOperation({
+    summary: 'Refresh Token',
+    description: 'Access Token과 Refresh Token 재생성',
+  })
   refresh(@Req() req: Request, @Res() res: Response) {
     const refreshToken: string | null = req.cookies.token;
     if (!refreshToken) throw new UnauthorizedException();
