4ac34f69f44cb72cefd1b2de83a56d4d5280cbfc
iam-green <yundohyun050121@gmail.com>
2025-02-19 02:18:46 +0900

    edit: `user` schema
    `discord_id` column 추가

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -4,7 +4,8 @@ import { z } from 'zod';
 
 export const userSchema = z.object({
   id: z.string().uuid(),
-  google_id: z.string().nullable(),
+  google_id: z.string().nullish(),
+  discord_id: z.string().nullish(),
   email: z.string().email(),
   created: z.coerce
     .date()
@@ -16,8 +17,19 @@ export class UserDto extends createZodDto(userSchema) {
   @ApiProperty({ description: 'User ID' })
   id: string;
 
-  @ApiProperty({ description: "User's Google ID", required: false })
-  google_id: string | null;
+  @ApiProperty({
+    description: "User's Google ID",
+    required: false,
+    nullable: true,
+  })
+  google_id: string | null | undefined;
+
+  @ApiProperty({
+    description: "User's Discord ID",
+    required: false,
+    nullable: true,
+  })
+  discord_id: string | null | undefined;
 
   @ApiProperty({ description: 'E-Mail' })
   email: string;
